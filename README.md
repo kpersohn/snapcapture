2c8fdace693d28807401137720261fd201c13b6b
Kyle Persohn <kyle.persohn@gmail.com>
2018-02-04 19:18:19 -0600

    Working MVP

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # snapcapture
+
+## Example settings.py
+
+```
+# Foscam
+# URL = 'http://ip address:port/cgi-bin/CGIProxy.fcgi?cmd=snapPicture2&usr=admin&pwd=xxx'
+
+# Nest
+# Use value from https://developer-api.nest.com/devices/cameras/device_id/snapshot_url
+# URL = 'https://developer-api.nest.com/string1/device_id/string2?auth=camera_token'
+```
--- snapcapture.py
+++ snapcapture.py
@@ -5,53 +5,49 @@ import urllib2
 from os import path
 
 
-def main():
-    ROOTDIR = path.basedir(path.abspath(__file__))
-    LOGFILE = path.join(ROOTDIR, 'log.txt')
+ROOTDIR = path.dirname(path.abspath(__file__))
+LOGFILE = path.join(ROOTDIR, 'log.txt')
 
-    logging.basicConfig(
-        filename=LOGFILE, format='%(asctime)s %(message)s', level=logging.DEBUG
-    )
+logging.basicConfig(
+    filename=LOGFILE, format='%(asctime)s %(message)s', level=logging.DEBUG
+)
 
-    try:
-        URL = None
-        execfile(path.join(ROOTDIR, 'settings.py'))
-    except StandardError:
-        logging.exception('Error loading settings.py')
-        raise
+try:
+    URL = None
+    execfile(path.join(ROOTDIR, 'settings.py'))
+except StandardError:
+    logging.exception('Error loading settings.py')
+    raise
 
-    dt = datetime.datetime.now()
-    savepath = path.join(
-        ROOTDIR,
-        dt.year,
-        '%02d'.format(dt.month),
-        '%02d'.format(dt.day)
-    )
-    savefile = path.join(savepath, dt.strftime('%Y%m%d-%H%M%S.jpg'))
+dt = datetime.datetime.now()
+savepath = path.join(
+    ROOTDIR,
+    'capture',
+    '{}'.format(dt.year),
+    '{:02d}'.format(dt.month),
+    '{:02d}'.format(dt.day)
+)
+savefile = path.join(savepath, dt.strftime('%Y%m%d-%H%M%S.jpg'))
 
-    try:
-        os.makedirs(savepath)
-    except OSError:
-        # Ignore if directory already exists.
-        pass
-    except StandardError:
-        logging.exception('Error creating download directory')
-        raise
+try:
+    os.makedirs(savepath)
+except OSError:
+    # Ignore if directory already exists.
+    pass
+except StandardError:
+    logging.exception('Error creating download directory')
+    raise
 
-    try:
-        remote = urllib2.urlopen(URL)
-        with open(savefile, 'wb') as local:
-            local.write(remote.read())
-    except urllib2.HTTPError:
-        logging.exception('HTTP error')
-        raise
-    except urllib2.HTTPError:
-        logging.exception('URL error')
-        raise
-    except StandardError:
-        logging.exception('Unspecified error')
-        raise
-
-
-if __name__ == '__main__':
-    main()
+try:
+    remote = urllib2.urlopen(URL)
+    with open(savefile, 'wb') as local:
+        local.write(remote.read())
+except urllib2.HTTPError:
+    logging.exception('HTTP error')
+    raise
+except urllib2.HTTPError:
+    logging.exception('URL error')
+    raise
+except StandardError:
+    logging.exception('Unspecified error')
+    raise
