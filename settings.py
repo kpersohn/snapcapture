1c7a27a2db5f1d8da09a2b79ecb3aea99a3a730d
Kyle Persohn <kyle.persohn@gmail.com>
2018-02-04 18:48:34 -0600

    Don't track settings.py

--- settings.py
+++
@@ -1,6 +0,0 @@
-# Example: Foscam
-# URL = 'http://ip address:port/cgi-bin/CGIProxy.fcgi?cmd=snapPicture2&usr=admin&pwd=xxx'
-
-# Example: Nest
-# Use value from https://developer-api.nest.com/devices/cameras/device_id/snapshot_url
-# URL = 'https://developer-api.nest.com/string1/device_id/string2?auth=camera_token'
